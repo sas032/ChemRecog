9774e5e21d5d32ba46ac86a195095ea5f875d11e
codiman <codiman@pop-os.localdomain>
2020-12-01 03:31:08 +0530

    inintal

---
+++ detector.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+img = cv2.imread()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #change to gray for canny detector
+edges = cv2.Canny(gray, 50, 150, apertureSize=3) #first thres = 50 second thres = 150
+cv2.imshow('edges', edges)
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 200) #store the output vector represented by (p,0,votes). 0 = line rotation
+
+for line in lines:
+    rho,theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    # x1 stores the rounded off value of (r * cos(theta) - 1000 * sin(theta))
+    x1 = int(x0 + 1000 * (-b))
+    # y1 stores the rounded off value of (r * sin(theta)+ 1000 * cos(theta))
+    y1 = int(y0 + 1000 * (a))
+    # x2 stores the rounded off value of (r * cos(theta)+ 1000 * sin(theta))
+    x2 = int(x0 - 1000 * (-b))
+    # y2 stores the rounded off value of (r * sin(theta)- 1000 * cos(theta))
+    y2 = int(y0 - 1000 * (a))
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+
+cv2.imshow('image', img)
+k = cv2.waitKey(0)
+cv2.destroyAllWindows()
