5190c671ea16579aaedac6657027a810eba02b8a
codiman <codiman@pop-os.localdomain>
2020-12-06 03:37:13 +0530

    changes

--- detector.py
+++ detector.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread()
+img = cv2.imread('one.jpeg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #change to gray for canny detector
 edges = cv2.Canny(gray, 50, 150, apertureSize=3) #first thres = 50 second thres = 150
+cv2.namedWindow('edges',cv2.WINDOW_NORMAL)
 cv2.imshow('edges', edges)
 lines = cv2.HoughLines(edges, 1, np.pi / 180, 200) #store the output vector represented by (p,0,votes). 0 = line rotation
 
@@ -23,7 +24,7 @@ for line in lines:
     y2 = int(y0 - 1000 * (a))
     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-
+cv2.namedWindow('image',cv2.WINDOW_NORMAL)
 cv2.imshow('image', img)
 k = cv2.waitKey(0)
 cv2.destroyAllWindows()
